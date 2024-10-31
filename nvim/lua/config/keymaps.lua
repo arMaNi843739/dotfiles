609c800006fde645a8fe34cf4d442b8203a7ef43
Yuki Yabumoto <fmsnow09@gmail.com>
2024-11-01 07:01:14 +0900

    commenting

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,10 +3,15 @@
 -- Add any additional keymaps here
 
 local map = LazyVim.safe_keymap_set
+
+-- change width size
 map("n", "<A-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
 map("n", "<A-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
 
 -- Hop (EasyMotion)
-local hop = require("hop")
-local directions = require("hop.hint").HintDirection
 map({ "n", "v" }, "f", "<cmd>HopWord<cr>", { desc = "HopWord" })
+
+-- comment.nvim
+vim.keymap.del("n", "<c-_>")
+local comment_api = require("Comment.api")
+vim.keymap.set("n", "<C-_>", comment_api.toggle.linewise.current)
