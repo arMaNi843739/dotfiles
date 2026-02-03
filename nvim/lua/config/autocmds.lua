5e65033f50fe3845eab26206e3f6731c8cc234d1
yyabumot <53650733+arMaNi843739@users.noreply.github.com>
2026-02-03 13:13:18 +0900

    update config to disable code format on save

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -2,9 +2,9 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- Disable autoformat for Vue files
+-- Disable autoformat for Vue files and Dockerfile
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "vue",
+  pattern = { "vue", "dockerfile" },
   callback = function()
     vim.b.autoformat = false
   end,
